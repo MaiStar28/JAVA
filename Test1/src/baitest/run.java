96f5ceac4c88998af912ece07af3c663fa458866
Meiiiii <hoangsaomai04@gmail.com>
2021-10-14 07:36:59 -0700

    test

---
+++ Test1/src/baitest/run.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package baitest;
+
+/**
+ *
+ * @author Admin
+ */
+public class run {
+    public static void main(String[] args) {
+        System.out.println("Mai");
+    }
+}
